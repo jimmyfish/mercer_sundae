2324637fa2e88792a711471497844868d1a20cb5
Dito YP <killcoder212@gmail.com>
2021-06-26 08:17:07 +0700

    add cron to process ticker by watch list

--- app/Console/Commands/Core/ProcessTickerDataCommand.php
+++ app/Console/Commands/Core/ProcessTickerDataCommand.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands\Core;
 
+use App\Http\Services\ClientRequesterService;
+use App\Models\Ticker;
+use App\Models\WatchList;
 use Illuminate\Console\Command;
 
 class ProcessTickerDataCommand extends Command
@@ -20,14 +23,18 @@ class ProcessTickerDataCommand extends Command
      */
     protected $description = 'Fetch ticker according to watch list';
 
+    private $requester;
+
     /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(ClientRequesterService $requester)
     {
         parent::__construct();
+
+        $this->requester = $requester;
     }
 
     /**
@@ -37,6 +44,37 @@ class ProcessTickerDataCommand extends Command
      */
     public function handle()
     {
-        //
+        $watchLists = WatchList::all();
+
+        $watchLists->each(function ($watchList) {
+            $symbol = $watchList->pair->symbol_id;
+            $response = $this->requester->indodax("ticker/$symbol");
+
+            $ticker = $response->getBody()->getContents();
+            $ticker = json_decode($ticker);
+            $ticker = collect($ticker->ticker);
+
+            $newTickerData = [];
+            foreach ($ticker as $key => $value) {
+                if (strpos($key, 'vol') !== false) {
+                    $symbolTruncated = str_replace("vol_", "", $key);
+
+                    if (strpos($symbol, $symbolTruncated) === 0) {
+                        $newTickerData['vol_left'] = $value;
+                    } else {
+                        $newTickerData['vol_right'] = $value;
+                    }
+
+                    continue;
+                }
+
+                if ($key === 'server_time') continue;
+
+                $newTickerData[$key] = $value;
+            }
+
+            $tickerData = new Ticker($newTickerData);
+            $tickerData->save();
+        });
     }
 }
